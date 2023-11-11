650d7223b2b412f4424bedac64fabbff07c874a5
Joshua Guerra <paul@zeevou.com>
2023-11-11 23:03:58 +0000

    event page

---
+++ pages/events/[eventId].js
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventCounts from "../../components/event-detail/event-content";
+import ErrorAlert from "@/components/ui/error-alert";
+function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p> No event found</p>
+      </ErrorAlert>
+    );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventCounts>
+        <p>{event.description}</p>
+      </EventCounts>
+    </Fragment>
+  );
+}
+
+export default EventDetailPage;
